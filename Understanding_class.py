61a1a3f684345a75f29c9aba74b18f617d6738b3
Hcrypt <37184669+Hcrypt@users.noreply.github.com>
2020-03-25 20:55:14 -0500

    Create Understanding_class.py

---
+++ Understanding_class.py
@@ -0,0 +1,18 @@
+class Person:
+  def __init__(self, name, age, hobby):
+    self.name = name
+    self.age = age
+    self.hobby = hobby
+
+p1 = Person("John", 18, "Programming")
+p2 = Person("Bob", 22, "Graphic Designing")
+
+print ("")
+print(p1.name)
+print(p1.age)
+print(p1.hobby)
+
+print("")
+print(p2.name)
+print(p2.age)
+print(p2.hobby)
